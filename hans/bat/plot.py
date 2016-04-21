10424461bbe72187b0a9a2ceaf3c81f31dbae3bf
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2016-04-21 14:37:16 +0200

    [bat] Add plot.py
    plot text output from bat analysis

---
+++ hans/bat/plot.py
@@ -0,0 +1,33 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.integrate import simps
+
+
+a = np.loadtxt("out.txt")
+
+# modify data to include the origin
+a = np.vstack((np.zeros(2), a))
+
+X = a.T[0]
+P = a.T[1]
+
+# check normalization of pdf using Simpson's rule
+norm = simps(P, X)
+print("Normalization", norm)
+
+# improve estimates by renormalizing to one
+P /= norm
+
+mean = simps(P * X, X)
+print("Mean", mean)
+print("std. dev", np.sqrt(simps(P * (X - mean)**2, X)))
+
+plt.plot(X, P)
+plt.xlabel("$X$")
+plt.ylabel("$P(X)$")
+
+plt.show()
+
+# Local Variables:
+# compile-command: "python3 plot.py"
+# End:
