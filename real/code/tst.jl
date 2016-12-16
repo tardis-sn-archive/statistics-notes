aee2436b983c25e4e9a64cbc70091f2843d85f3c
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2016-12-16 16:19:39 +0100

    [code] Minore playing around

--- real/code/tst.jl
+++ real/code/tst.jl
@@ -1,66 +1,38 @@
-import optim
-import PyCall
+import Distributions, PyCall, TardisStats
+using FredPlots
 
-function main()
-    # define order of linear models
-    αOrder = 1
-    βOrder = 1
+α0 = 1.5; β0 = 60.; λ0 = 500
+νmin=0.
+νmax=10.
 
-    # read in data and create posterior
-    dim = αOrder + βOrder
-    ∇res, Hres = optim.allocations(dim)
-    frame, (P, ∇P!, HP!) = optim.problem(αOrder=αOrder, βOrder=βOrder, run=99)
+# define order of linear models
+αOrder = 1
+βOrder = 1
+npackets=1000
 
-    # compute the posterior mode to estimate the evidence
-    @time maxP, mode, ret = optim.run_nlopt(frame, P, ∇P!, HP!, αOrder, βOrder, xtol_rel=1e-3)
-    HP!(mode, Hres)
-    evidence = optim.laplace(maxP, Hres)
-    println("got $maxP at $mode (returned $ret) and evidence $evidence")
+# read in data from tardis and create posterior
+dim = αOrder + βOrder
+∇res, Hres = TardisStats.optim.allocations(dim)
+dataframe, (P, ∇P!, HP!) = TardisStats.optim.problem(αOrder=αOrder, βOrder=βOrder, run=99, npackets=npackets)
 
-    # the posterior mean with the normalized posterior
-    pm = optim.PosteriorMean(0.1, 0.2, 6.0)
-    Pmean, ∇Pmean!, HPmean! = optim.targetfactory(frame, αOrder, βOrder, evidence=evidence, pm=pm)
-    println(Pmean(mode))
-    @time maxPmean, mode, ret = optim.run_nlopt(frame, Pmean, ∇Pmean!, HPmean!, αOrder, βOrder,
-                                          init=mode, xtol_rel=1e-8)
-    println("max of normalized posterior = $maxPmean at $mode")
+# now change the energies
+PyCall.@pyimport numpy.random as nprand
+nprand.seed(1358)
+PyCall.@pyimport scipy.stats as stats
+dist0 = stats.gamma(α0, scale=1/β0)
+# crucial: change dataframe in place, else optimization runs with old energies!
+dataframe[:energies][:] = dist0[:rvs](size=length(dataframe[:energies]))
+# Plots.histogram(dataframe[:energies])
 
-    return
+# the observed number of packets in the bin
+nmin=searchsortedfirst(dataframe[:nus], νmin)
+nmax=searchsortedlast(dataframe[:nus], νmax)
+n = nmax - nmin + 1
+x = sum(dataframe[:energies][nmin:nmax])
+n,x
 
-    nb = NegBinom(5, 0.5)
-    Pall, ∇Pall!, HPall! = targetfactory(frame, αOrder, βOrder, evidence=evidence, pm=pm, nb=nb)
-    @time maxPall, mode, ret = run_nlopt(frame, Pall, ∇Pall!, HPall!, αOrder, βOrder,
-                                          init=vcat(mode, 10), xtol_rel=1e-8, nb=nb)
-    println("max of normalized all posterior = $maxPall at $mode")
-    # return
-
-    points = collect(linspace(0.0, 0.6, 25))
-    results = zeros(points)
-    # skip X = 0
-    optimize = false
-    for i in 2:length(points)
-        pm.X = points[i]
-        results[i] = optim.predict_small(frame, Pmean, ∇Pmean!, HPmean!, pm, αOrder, βOrder, mode, nb, optimize=optimize)
-    end
-
-    # self-normalize estimates through Simpson's rule
-    PyCall.@pyimport scipy.integrate as si
-    norm = si.simps(results, points)
-    println("norm = $norm")
-    results /= norm
-    println(results)
-
-    Plots.plot(points, results)
-fname =
-    Plots.pdf("test_$(optimize? "opt" : "noopt").pdf")
-
-    # # update X
-    # pm.N = 7
-    # println(Pmean(mode))
-    # @time maxPmean, mode, ret = run_nlopt(frame, Pmean, ∇Pmean!, HPmean!, αOrder, βOrder,
-    #                                       init=mode, xtol_rel=1e-8)
-    # println("max of normalized posterior = $maxPmean at $mode")
-
-end
-
-main()
+@time maxP, posterior_mode, ret = TardisStats.optim.run_nlopt(dataframe, P, ∇P!, HP!,
+    αOrder, βOrder, xtol_rel=1e-4, init=1.001*[α0, β0])
+HP!(posterior_mode, Hres)
+evidence = TardisStats.optim.laplace(maxP, Hres)
+println("got $maxP at $(posterior_mode) (returned $ret) and evidence $evidence")
