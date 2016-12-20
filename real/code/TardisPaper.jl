acb652be9f43589b2817831e06647dae17770ad0
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2016-12-20 14:10:34 +0100

    [code] add Integration module with Cubature

--- real/code/TardisPaper.jl
+++ real/code/TardisPaper.jl
@@ -1,10 +1,77 @@
 module TardisPaper
 
+"""use Cubature or Laplace"""
+module Integrate
+using Cubature
+
+function by_cubature(logf::Function, xmin::Real, xmax::Real; kwargs...)
+    # count function evaluations
+    counter = 0
+    h = t -> (counter += 1; exp(logf(t)))
+
+    # transform variables
+    if xmax === Inf
+        h = t -> (counter += 1; exp(logf(xmin + t/(1-t))) * 1/(1-t)^2)
+        xmin, xmax = 0, 1
+    end
+    estimate, σ = hquadrature(h, xmin, xmax; kwargs...)
+    return estimate, σ, counter
+end
+
+function by_cubature(logf::Function, xmin::Vector, xmax::Vector; kwargs...)
+    # count function evaluations
+    counter = 0
+
+    # keep independent limits in case of transform
+    a = copy(xmin)
+    b = copy(xmax)
+
+    # count parameters to transform and their index in the arg vector
+    ninf = sum(x -> x == Inf, xmax)
+    indices = Vector{Int16}(ninf)
+    offset = 1
+    for (i, xi) in enumerate(xmax)
+        if xi == Inf
+            indices[offset] = i
+            offset += 1
+            a[i] = 0
+            b[i] = 1
+        end
+    end
+
+    if ninf == 0
+        h = function(t) counter += 1; exp(logf(t)) end
+    else
+        # transform variables, mutate x in place
+        x = Vector(xmax)
+        trafo = function(t)
+            x .= t
+            jacobian = 1.0
+            for i in indices
+                x[i] = xmin[i] + t[i]/(1-t[i])
+                jacobian *= 1/(1-t[i])^2
+            end
+            return jacobian
+        end
+        h = function(t)
+            counter += 1
+            jacobian = trafo(t)
+            exp(logf(x)) * jacobian
+        end
+    end
+    estimate, σ = hcubature(h, a, b; kwargs...)
+    return estimate, σ, counter
+end
+
+end # Integrate
+
+
 """reload("TardisPaper"); TardisPaper.GammaIntegrand.test()"""
 module GammaIntegrand
 
 export log_posterior, make_log_posterior
 
+import ..Integrate
 using Base.Test, Distributions, Optim
 
 """ posterior P(α, β | n, q, r) for the parameters of the Gamma
@@ -62,6 +129,24 @@ function test()
     # only finite accuracy in max-likelihood
     @test isapprox(Optim.minimizer(res)[1], α; rtol=1e-3)
     @test isapprox(Optim.minimizer(res)[2], β; rtol=5e-2)
+
+    # integral over prediction should be normalized in 1D...
+
+    estimate, σ, ncalls = Integrate.by_cubature(Q -> log_predict(Q, α, β, n), 0, Inf; reltol=1e-6)
+    println("ncalls $ncalls")
+    @test isapprox(estimate, 1; atol=σ)
+
+    # and 2D for large but finite upper limits ...
+    estimate, σ, ncalls = Integrate.by_cubature(x -> log_predict(x[1], α, β, n) + log_predict(x[2], α, β, n),
+                                                [0.0, 0.0], [500.0, 500.0]; reltol=1e-6)
+    println("After $ncalls calls: $estimate ± $σ")
+    @test isapprox(estimate, 1; atol=σ)
+
+    # and infinite upper limits.
+    estimate, σ, ncalls = Integrate.by_cubature(x -> log_predict(x[1], α, β, n) + log_predict(x[2], α, β, n),
+                                                [0.0, 0.0], [Inf, Inf]; reltol=1e-6)
+    println("After $ncalls calls: $estimate ± $σ")
+    @test isapprox(estimate, 1; atol=σ)
 end
 
 end # GammaIntegrand
