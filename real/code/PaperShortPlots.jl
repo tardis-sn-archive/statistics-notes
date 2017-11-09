d9d5eb50cd3a835be882cd94fa3c999ae76778e7
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2017-11-09 11:05:22 +0100

    [ShortPlots] define data parameters

--- real/code/PaperShortPlots.jl
+++ real/code/PaperShortPlots.jl
@@ -1,8 +1,12 @@
 # reload("TardisPlotUtils")
 using Plots
 import TardisPaper.Predict
+using Distributions
+
+α=1.5; β=60
+μ0 = α/β; σ20 = α/β^2
+k=34; data = rand(dist, k);
+meanℓ = mean(data); varℓ=var(data; corrected=false)
 
 
-
-
-Plots.gr()
+# Plots.gr()
