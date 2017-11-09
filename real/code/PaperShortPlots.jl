0135fbc6f3a0daf3e9d201f538e61cff0d34ca80
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2017-11-09 15:21:20 +0100

    (wip) create plots

--- real/code/PaperShortPlots.jl
+++ real/code/PaperShortPlots.jl
@@ -1,12 +1,7 @@
 # reload("TardisPlotUtils")
-using Plots
-import TardisPaper.Predict
-using Distributions
 
-α=1.5; β=60
-μ0 = α/β; σ20 = α/β^2
-k=34; data = rand(dist, k);
-meanℓ = mean(data); varℓ=var(data; corrected=false)
-
-
+# α=1.5; β=60
+# μ0 = α/β; σ20 = α/β^2
+# dist = Gamma(α, 1/β)
+plot_fixed()
 # Plots.gr()
